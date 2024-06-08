9c992f24771af89fc1e0a3da894bea0165a91507
HasmigG <hasmigg23@gmail.com>
2024-06-08 16:51:05 -0700

    complete

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,86 +3,55 @@ import csv
 
 # Path to collect data from the Resources folder
 budget_data = os.path.join('Resources', 'budget_data.csv')
+my_report = open('Analysis/Budget_Analysis.txt','w')
 
 months = 0
-total_profit_loss = 0
-previous_profit_loss = False
+total = 0
+previous_revenue = 0
 total_change = 0
+increase = ["",0]
+decrease = ["",0]
 
-changes_list = []
-inc = ["",0]
-dec = ["",0]
+with open(budget_data, 'r') as file:
+    csvreader = csv.reader(file)
+    csv_header = next(csvreader)
 
-with open(budget_data) as data:
-    reader = csv.reader(data)
-
-    # Read the header row
-    # header = next(reader)
-    next(reader)
-   
-    for row in reader:
-        months = months + 1
-        
+    for row in csvreader:
+        months += 1
+            
         # Total
         profit_loss = int(row[1])
-        total_profit_loss += profit_loss
+        total += profit_loss
 
-        if previous_profit_loss: #if it's anything that isn't false
-                
-            # # Average change from EXPERT
-            change = profit_loss - previous_profit_loss
-            changes_list.append(change)
-            
-             # Greatest Increase
-            if change > inc[1]:
-                inc[0] = row[0]
-                inc[1] = change
+        # Average Change
+        change = profit_loss - previous_revenue
+        if previous_revenue == 0:
+            change = 0
+        
+        total_change += change
 
-            # Greatest Decrease
-            if change < dec[1]:
-                dec[0] = row[0]
-                dec[1] = change
+        previous_revenue = profit_loss
 
-        previous_profit_loss = profit_loss
+        # Greatest Increase
+        if change > increase[1]:
+            increase[0] = row[0]
+            increase[1] = change
 
-    
+        # Greatest Decrease
+        if change < decrease[1]:
+            decrease[0] = row[0]
+            decrease[1] = change
 
-                # # Average change from TUTOR    
-                # change =  profit_loss - previous_profit_loss
-                # if previous_profit_loss == 0:
-                #     change = 0
+output = f'''
+Financial Analysis
+----------------------------
+Total Months: {months}
+Total: ${total:,}
+Average Change: ${total_change/(months-1):,.2f}
+Greatest Increase in Profits: {increase[0]} (${increase[1]:,})
+Greatest Decrease in Profits: {decrease[0]} (${decrease[1]:,})
+'''
 
-                # total_change += change
-
-                # previous_profit_loss = profit_loss
-    
-
-    average_change = sum(changes_list) / len(changes_list)
-    print(f"Testing: {sum(changes_list)}")
-
-
-# Print out the state's name and their graduation rates
-print(f"Financial Analysis")
-print(f"----------------------------")
-print(f"Total Months: 86 {months}")
-print(f"Total: $22564198 ${total_profit_loss}")
-# print(f"Average Change: $-8311.11 ${average_change}")
-print(f"Average Change: $-8311.11 ${average_change()}")
-print(f"Greatest Increase in Profits: Aug-16 ($1862002) {inc[0]} (${inc[1]})")
-print(f"Greatest Decrease in Profits: Feb-14 ($-1825558) {dec[0]} (${dec[1]})")
-
- 
-#  with open('Analysis', "financial_analysis.txt", 'w'):
-
-#     # Write the rows
-        # csvwriter.writerow(f"Financial Analysis")
-        # csvwriter.writerow(f"----------------------------")
-        # csvwriter.writerowf"Total Months: 86 {months}")
-        # csvwriter.writerow(f"Total: $22564198 ${total_profit_loss}")
-
-        # csvwriter.writerow(f"Average Change: $-8311.11 ${average_change}")
-
-        # csvwriter.writerow(f"Greatest Increase in Profits: Aug-16 ($1862002) {inc[0]} (${inc[1]})")
-        # csvwriter.writerow(f"Greatest Decrease in Profits: Feb-14 ($-1825558) {dec[0]} (${dec[1]})")
-
-
+print (output)
+my_report.write(output)
+my_report.close()
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,63 +1,54 @@
-import os
 import csv
 
-# Path to collect data from the Resources folder
-election_data = os.path.join('Resources', 'election_data.csv')
+# open file in dictionary reader mode
+data = csv.DictReader(open('Resources/election_data.csv'))
+
+# open a new text file to receive the output
 my_report = open("Analysis/Election_Analysis.txt", "w")
 
+# set up variables
 total = 0
 candidate_list = {}
 win_votes = 0
 
-for row in election_data:
+# for loop to get data from each row in the file
+for row in data:
+    # increment the total votes
     total += 1
 
-    canditate = row["Candidate"]
-    if canditate not in candidate_list.keys():
+    # candidate's name from current row
+    candidate = row["Candidate"]  
+
+    # get new candidate name with 0 votes
+    if candidate not in candidate_list.keys():
         candidate_list[candidate] = 0
     candidate_list[candidate] += 1
-
-
+   
+# set up for the output
 output = f'''
+
 Election Results
 -------------------------
-Total Votes: {total}
+Total Votes: {total:,}
 -------------------------
 '''
 
+# for loop to get votes per candidate
 for candidate in candidate_list.keys():
-    votes = can_list[can]
+    votes = candidate_list[candidate]
 
+    # calculate percentage of votes per candidate and store in variable for output
     output += f'{candidate}: {votes/total *100:.3f}% ({votes:,})\n'
 
+    # check if subsequent candidate has more votes than the previous
     if votes > win_votes:
         win_votes = votes
         winner = candidate
 
+# add winner to output variable
 output += f'------------------------- \nWinner: {winner}\n-------------------------'
 
+# print to console
 print(output)
+# output to text file
 my_report.write(output)
-
-#
-# Charles Casper Stockham: 23.049% (85213)
-# Diana DeGette: 73.812% (272892)
-# Raymon Anthony Doane: 3.139% (11606)
-# -------------------------
-# Winner: Diana DeGette
-# -------------------------
-    
-
-#  with open('Analysis', "election_result.txt", 'w'):
-
-#     # Write the rows
-        # csvwriter.writerow(f"Financial Analysis")
-        # csvwriter.writerow(f"----------------------------")
-        # csvwriter.writerowf"Total Months: 86 {months}")
-        # csvwriter.writerow(f"Total: $22564198 ${total_profit_loss}")
-
-        # csvwriter.writerow(f"Average Change: $-8311.11 ${average_change}")
-
-        # csvwriter.writerow(f"Greatest Increase in Profits: Aug-16 ($1862002) {inc[0]} (${inc[1]})")
-        # csvwriter.writerow(f"Greatest Decrease in Profits: Feb-14 ($-1825558) {dec[0]} (${dec[1]})")
-
